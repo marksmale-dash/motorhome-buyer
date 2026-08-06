b4d1edd25308930256898fb4132867fea9947aba
marksmale-dash <markjsmale@googlemail.com>
2026-08-06 08:41:18 +0100

    Add secure household sharing (v4.2.0)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='motorhome-compass-v4.1.0';
+const CACHE='motorhome-compass-v4.2.0';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==self.location.origin)return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||(e.request.mode==='navigate'?caches.match('./index.html'):undefined))));});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==self.location.origin)return;const cacheKey=e.request.mode==='navigate'?new Request(new URL('./index.html',self.location).toString()):e.request;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(cacheKey,copy));return r;}).catch(()=>caches.match(cacheKey).then(r=>r||(e.request.mode==='navigate'?caches.match('./index.html'):undefined))));});
